8954455d7ef672c39460feb504926b10fa575632
FelipeBiten <bavierart@gmail.com>
2025-08-05 02:54:34 -0300

    Project setup and basics

---
+++ portfolio_frontend/src/components/Project.jsx
@@ -0,0 +1,11 @@
+import './styles_components/Project.css'
+
+export default function Project(props) {
+    return (
+        <>
+            <h3></h3>
+            <img src="" alt="" />
+            <p></p>
+        </>
+    )
+}
